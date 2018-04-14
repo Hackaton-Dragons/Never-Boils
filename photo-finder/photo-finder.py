af5f2f958f89a6c6e90c5801f2bbb4c4da3a72f6
Noah Cote <noah.cote3@gmail.com>
2018-04-14 13:40:02 -0400

    camera input updated

--- photo-finder/photo-finder.py
+++ photo-finder/photo-finder.py
@@ -2,7 +2,7 @@ import pygame.camera
 import pygame.image
 def main():
     pygame.camera.init()
-    cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+    cam = pygame.camera.Camera(pygame.camera.list_cameras()[1])
     cam.start()
     img = cam.get_image()
     pygame.image.save(img, "photo.bmp")
